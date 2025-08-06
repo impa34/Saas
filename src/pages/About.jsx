47620dd9352b7732fd5383da2de718472fe2608c
Bogdan Chayka <boom2002boomyt@gmail.com>
2025-08-06 15:18:28 +0200

    improved landing page and solved some bugs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,28 +22,38 @@ function About() {
       <div className="flex-1 px-6 py-4 flex flex-col items-center justify-center mb-20">
         <div className="max-w-3xl text-center">
           <h1 className="text-4xl font-bold mb-6 text-purple-500">
-  Sobre Talo Chatbot
-</h1>
-<p className="text-lg text-gray-300 mb-4">
-  Talo Chatbot es un asistente con inteligencia artificial que ayuda a tu negocio
-  a conectar con los clientes de forma más inteligente y eficiente.
-</p>
-<p className="text-gray-400 mb-6">
-  Nuestra misión es simplificar el soporte y la automatización mediante conversaciones naturales e inteligentes.
-  Ya seas un pequeño equipo o una gran empresa, Talo se adapta a tus necesidades.
-  Disfruta de integraciones fluidas como Google Calendar o Excel, junto con una interfaz completamente personalizable para adaptarse a tu marca.
-</p>
+            Sobre Talo Chatbot
+          </h1>
+          <p className="text-lg text-gray-300 mb-4">
+            Talo Chatbot es un asistente con inteligencia artificial que ayuda a
+            tu negocio a conectar con los clientes de forma más inteligente y
+            eficiente.
+          </p>
+          <p className="text-gray-400 mb-6">
+            Nuestra misión es simplificar el soporte y la automatización
+            mediante conversaciones naturales e inteligentes. Ya seas un pequeño
+            equipo o una gran empresa, Talo se adapta a tus necesidades.
+            Disfruta de integraciones fluidas como Google Calendar o Excel,
+            junto con una interfaz completamente personalizable para adaptarse a
+            tu marca.
+          </p>
 
-<h1 className="text-4xl font-bold mb-6 text-purple-500 py-2 mt-2">
-  ¿Cómo funciona?
-</h1>
-<p className="text-gray-400">
-  Empezar es muy fácil: solo nombra tu bot, añade las preguntas frecuentes que
-  puedan hacer tus clientes y proporciona las respuestas. No necesitas respuestas
-  perfectas, ya que la IA se encarga de entender variaciones y matices.
-  Con Talo, puedes ofrecer atención 24/7 sin contratar más personal, haciendo tu negocio
-  más escalable y eficiente.
-</p>
+          <h1 className="text-4xl font-bold mb-6 text-purple-500 py-2 mt-2">
+            ¿Cómo funciona?
+          </h1>
+          <p className="text-gray-400">
+            Empezar es muy fácil: solo nombra tu bot, añade las preguntas
+            frecuentes que puedan hacer tus clientes y proporciona las
+            respuestas. No necesitas respuestas perfectas, ya que la IA se
+            encarga de entender variaciones y matices. Con Talo, puedes ofrecer
+            atención 24/7 sin contratar más personal, haciendo tu negocio más
+            escalable y eficiente.
+          </p>
+          <p className="text-gray-400 mb-6">
+            El plan gratuito te da un chatbot y
+            cinco preguntas para que veas como funciona, puedes
+            ampliar tu suscripción cuando lo desees.
+          </p>
         </div>
       </div>
 
