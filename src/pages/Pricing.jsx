434e48440e3c141455521473cb62f707606c7d31
Bogdan Chayka <boom2002boomyt@gmail.com>
2025-08-17 10:22:14 +0200

    .

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -76,7 +76,7 @@ export default function Pricing() {
 
   try {
     const res = await fetch(
-      "https://saas-backend-xrkb.onrender.com/api/paypal/create-order",
+      "https://saas-backend-xrkb.onrender.com/api/paypal/capture-order",
       {
         method: "POST",
         headers: {
