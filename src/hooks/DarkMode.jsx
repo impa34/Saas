8a844d323731a92b0e97f04b495d0e1ddf27006a
Bogdan Chayka <boom2002boomyt@gmail.com>
2025-08-18 19:05:40 +0200

    added dark mode

--- src/hooks/DarkMode.jsx
+++ src/hooks/DarkMode.jsx
@@ -1,19 +1,22 @@
-// src/hooks/useDarkMode.js
 import { useEffect, useState } from "react";
 
 export default function useDarkMode() {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") || "dark"
-  );
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme") || "dark"; // por defecto dark
+    }
+    return "dark";
+  });
 
   useEffect(() => {
     const root = window.document.documentElement;
     if (theme === "dark") {
       root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
       root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
-    localStorage.setItem("theme", theme);
   }, [theme]);
 
   return [theme, setTheme];
