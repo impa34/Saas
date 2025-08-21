d14a39fc540638c540c11a9da901ae343c5211db
Bogdan Chayka <boom2002boomyt@gmail.com>
2025-08-21 10:07:41 +0200

    .

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,20 +1,13 @@
-// src/components/ThemeToggle.jsx
-import { Moon, Sun } from "lucide-react";
-import useDarkMode from "../hooks/useDarkMode";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+import { Sun, Moon } from "lucide-react"; // opcional iconos
 
 function ThemeToggle() {
-  const [theme, setTheme] = useDarkMode();
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <button
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="ml-4 p-2 rounded-lg bg-gray-200 dark:bg-gray-700 transition"
-    >
-      {theme === "dark" ? (
-        <Sun className="w-5 h-5 text-yellow-400" />
-      ) : (
-        <Moon className="w-5 h-5 text-gray-900" />
-      )}
+    <button onClick={toggleTheme} className="p-2 rounded-lg">
+      {theme === "dark" ? <Sun /> : <Moon />}
     </button>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AuthProvider } from "./context/AuthProvider";
 import App from "./App.jsx";
+import { ThemeProvider } from "./context/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </BrowserRouter>
 );
