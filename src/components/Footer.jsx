d6c8cd6a8938019e84e69eed2edc0d463a3d7e00
Bogdan Chayka <boom2002boomyt@gmail.com>
2025-08-23 19:27:41 +0200

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 function Footer() {
   return (
     <footer className="text-center py-6 text-gray-400 text-sm bg-white dark:bg-gray-900">
+      <script src="http://www.talochatbot.com/widget.js" data-chatbot-id="686e55ac28801949c4df3ba9" ></script>
   <div className="mb-2 space-x-4">
     <Link to="/pricing" className="hover:text-gray-900 dark:text-white">Planes</Link>
     <Link to="/about" className="hover:text-gray-900 dark:text-white">Acerca de</Link>
