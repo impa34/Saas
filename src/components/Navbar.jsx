eef5b5eb2fe2cc7a81c432bb2ecd3b58fb8eddca
Bogdan Chayka <boom2002boomyt@gmail.com>
2025-08-29 10:54:25 +0200

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useNavigate, Link } from "react-router-dom";
+import {useState} from "react"
 import { useAuth } from "../context/AuthProvider";
 import { Globe } from "lucide-react";
 import ThemeToggle from "./ThemeToggle";
